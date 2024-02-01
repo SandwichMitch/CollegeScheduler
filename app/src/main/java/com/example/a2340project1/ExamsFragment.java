18b06ea1f7d88e6b195aec6361bfbf19ac69785e
Ryan Ruiming Li <ryan.ruiming.li@gmail.com>
2024-01-31 23:53:13 -0500

    edited java classes

--- app/src/main/java/com/example/a2340project1/ExamsFragment.java
+++ app/src/main/java/com/example/a2340project1/ExamsFragment.java
@@ -2,6 +2,7 @@ package com.example.a2340project1;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -61,4 +62,10 @@ public class ExamsFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_exams, container, false);
     }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+
 }
